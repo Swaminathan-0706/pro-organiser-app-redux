5a9c92702d54e21aee085899a3bc5684ed4364fd
Swaminathan <sayeeiyer@gmail.com>
2020-07-19 23:15:06 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/NavBar/Navbar';
-import { Switch, Route} from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import CreateBoard from './Pages/CreateBoard/CreateBoard';
-import Boards from './Pages/Boards/Boards';
+
 
 
 
@@ -12,11 +9,6 @@ function App() {
   return (
     <>
       <Navbar/>
-      <Switch>
-      <Route exact path='/' component={Home} />
-      <Route strict path='/createboard' component={CreateBoard} />
-      <Route path='/board/:boardid' component={Boards} />
-      </Switch>
     </>
   );
 }
--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import styles from './Navbar.module.scss';
+import React, { Component } from 'react'
+import styles from './Navbar.module.css';
 import {NavLink} from 'react-router-dom';
+
 class Navbar extends Component {
     render() {
         return (
-        <nav className={styles.navbar}>
-
-            <div className={styles.title}>
-               <h2>Pro-Organizer</h2>
-
-            </div>
-            <ul className={styles.lists}>
-               <NavLink id="list1" className={styles.links} exact activeClassName={styles.active} to="/">Home</NavLink> 
-               <NavLink id="list2" className={styles.links} activeClassName={styles.active} to="/createboard">Create a Board</NavLink> 
-               
-            </ul>
-        </nav>
+            <header className={styles.header}>
+             <h1>Pro-Organizer</h1> 
+             <ul className={styles.listCtr}>
+                 <NavLink exact to='/' className={styles.listItem} activeClassName={styles.active}>Home</NavLink>
+                 <NavLink to='/createboard' className={styles.listItem} activeClassName={styles.active}>Create a Board</NavLink>
+             </ul> 
+            </header>
         )
     }
 }
